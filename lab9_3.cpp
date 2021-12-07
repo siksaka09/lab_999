8429a3ac5c60beacb3989f6d59d18df68ae0ec34
siksaka09 <earthsik09@gmail.com>
2021-12-07 17:54:09 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age,height,property;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age <= 20)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height <160)
+        {
+            cout << "Your status = UNFRIEND";
+        }
+        else if( height <175)
+        {
+            cout << "Your status = FRIEND";
+        }
+        else
+        {
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property <= 69000000)
+            {
+                cout << "Your status = ONE-NIGHT-STAND";
+            }
+            else
+            {
+                cout << "Your status = MARRIED";
+            }
+
+        }
+
+    }
+    else if(age < 30)
+    {
+        cout << "Enter your property: ";
+        cin >> property;
+
+        if(property > 10000000)
+        {
+            cout << "Your status = BEST FRIEND";
+        }
+        else
+        {
+            cout << "Your status = UNFRIEND";
+
+        }
+    }
+    else
+    {
+        cout << "Your status = UNFRIEND";
+    }
+
+}
+
+
 /*
 "Enter your age: "
 "Enter your height: "
